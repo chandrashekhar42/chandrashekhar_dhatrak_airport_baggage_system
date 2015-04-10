02f7cd380d5fca64861b33fccf9c7a994b9941f3
chandrashekhar42 <chandrashekhar42@gmail.com>
2015-04-11 00:26:03 +0530

    ApplicationConstants.java

---
+++ ApplicationConstants.java
@@ -0,0 +1,21 @@
+package com.tutorial.util;
+
+
+public class ApplicationConstants
+{
+	public static final String A1 = "A1";
+	public static final String A2 = "A2";
+	public static final String A3 = "A3";
+	public static final String A4 = "A4";
+	public static final String A5 = "A5";
+	public static final String A6 = "A6";
+	public static final String A7 = "A7";
+	public static final String A8 = "A8";
+	public static final String A9 = "A9";
+	public static final String A10 = "A10";
+	
+	public static final String BAGGAGE_CLAIM = "BAGGAGE_CLAIM";
+	public static final String TICKET_SYSTEM = "TICKET_SYSTEM";
+	
+	public static final String ARRIVAL = "ARRIVAL";
+}
